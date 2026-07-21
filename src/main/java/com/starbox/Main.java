a4310d6855c1c4d0bbcbec87156f214fd828b5b0
Thomas Elder <thombsaway@gmail.com>
2026-07-21 12:19:56 +1000

    Simplify main class, using compact source file syntax

--- src/main/java/com/starbox/Main.java
+++ src/main/java/com/starbox/Main.java
@@ -1,16 +1,8 @@
-package com.starbox;
 
 import com.starbox.io.GameWindow;
 
 import javax.swing.SwingUtilities;
 
-public final class Main {
-
-    private Main() {
-        // no instances
-    }
-
-    static void main() {
-        SwingUtilities.invokeLater(GameWindow::launch);
-    }
+void main() {
+   SwingUtilities.invokeLater(GameWindow::launch);
 }
